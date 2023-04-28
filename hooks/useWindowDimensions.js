12bc92d75d2993be0c5ed7435a2af07121e3da1c
Drakulus <khuatdangquang11a9@gmail.com>
2023-04-28 10:58:12 +0700

    fix window dimenstions

--- hooks/useWindowDimensions.js
+++ hooks/useWindowDimensions.js
@@ -1,32 +1,29 @@
-import { useState, useEffect } from 'react';
+import { useEffect, useState } from 'react';
 
-function getWindowDimensions() {
-  if (typeof window !== 'undefined') {
-    const { innerWidth: width, innerHeight: height } = window;
-    return {
-      width,
-      height,
-    };
-  }
-}
-
-export function useWindowDimensions() {
-  const [windowDimensions, setWindowDimensions] = useState(getWindowDimensions());
-
+// Hook
+function useWindowDimensions() {
+  // Initialize state with undefined width/height so server and client renders match
+  // Learn more here: https://joshwcomeau.com/react/the-perils-of-rehydration/
+  const [windowSize, setWindowSize] = useState({
+    width: undefined,
+    height: undefined,
+  });
   useEffect(() => {
+    // Handler to call on window resize
     function handleResize() {
-      if (typeof window !== 'undefined') {
-        setWindowDimensions(getWindowDimensions());
-      }
+      // Set window width/height to state
+      setWindowSize({
+        width: window.innerWidth,
+        height: window.innerHeight,
+      });
     }
-
-    if (typeof window !== 'undefined') {
-      window.addEventListener('resize', handleResize);
-      return () => window.removeEventListener('resize', handleResize);
-    }
-  }, []);
-
-  return windowDimensions;
+    // Add event listener
+    window.addEventListener('resize', handleResize);
+    // Call handler right away so state gets updated with initial window size
+    handleResize();
+    // Remove event listener on cleanup
+    return () => window.removeEventListener('resize', handleResize);
+  }, []); // Empty array ensures that effect is only run on mount
+  return windowSize;
 }
-
 export default useWindowDimensions;

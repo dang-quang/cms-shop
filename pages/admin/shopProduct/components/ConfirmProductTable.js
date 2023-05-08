1d82e7e2a2c3b8c3a6aab207478f8a0389a74398
Drakulus <khuatdangquang11a9@gmail.com>
2023-05-08 17:55:13 +0700

    commit

--- pages/admin/shopProduct/components/ConfirmProductTable.js
+++ pages/admin/shopProduct/components/ConfirmProductTable.js
@@ -46,99 +46,101 @@ const ConfirmProductTable = (props) => {
       overflowX={{ sm: 'scroll', lg: 'hidden' }}>
       <Table {...getTableProps()} variant="simple" color="gray.500">
         <Thead>
-          {headerGroups.map((headerGroup, index) => (
-            <Tr {...headerGroup.getHeaderGroupProps()} key={index}>
-              {headerGroup.headers.map((column, index) => (
-                <Th
-                  {...column.getHeaderProps(column.getSortByToggleProps())}
-                  pe="10px"
-                  key={index}
-                  borderColor="gray.1600"
-                  pl="32px"
-                  pr="20px">
-                  <Flex align="center" fontSize={{ sm: '10px', lg: '12px' }} color="text-gray-6">
-                    <Text textStyle="h3-t" textTransform="capitalize" color="primary.200">
-                      {column.render('Header')}
-                    </Text>
-                  </Flex>
-                </Th>
-              ))}
-            </Tr>
-          ))}
+          {headerGroups.length > 0 &&
+            headerGroups.map((headerGroup, index) => (
+              <Tr {...headerGroup.getHeaderGroupProps()} key={index}>
+                {headerGroup.headers.map((column, index) => (
+                  <Th
+                    {...column.getHeaderProps(column.getSortByToggleProps())}
+                    pe="10px"
+                    key={index}
+                    borderColor="gray.1600"
+                    pl="32px"
+                    pr="20px">
+                    <Flex align="center" fontSize={{ sm: '10px', lg: '12px' }} color="text-gray-6">
+                      <Text textStyle="h3-t" textTransform="capitalize" color="primary.200">
+                        {column.render('Header')}
+                      </Text>
+                    </Flex>
+                  </Th>
+                ))}
+              </Tr>
+            ))}
         </Thead>
         <Tbody {...getTableBodyProps()} gap="20px">
-          {page.map((row, index) => {
-            prepareRow(row);
-            return (
-              <Tr {...row.getRowProps()} key={index} h="20px">
-                {row.cells.map((cell, index) => {
-                  let data;
-                  if (cell.column.Header === t('name')) {
-                    data = (
-                      <Flex>
-                        <Image
-                          borderRadius="0px"
-                          objectFit="cover"
-                          src={cell.value.image}
-                          w="85px"
-                          h="62px"
-                          mr="3"
-                        />
-                        <Box>
-                          <Text color="text-basic" textStyle="h3">
-                            {cell.value.name}
-                          </Text>
-                          <Text textStyle="h2" color="text-body" mt="1">
-                            {cell.value.code}
-                          </Text>
-                        </Box>
-                      </Flex>
+          {page.length > 0 &&
+            page.map((row, index) => {
+              prepareRow(row);
+              return (
+                <Tr {...row.getRowProps()} key={index} h="20px">
+                  {row.cells.map((cell, index) => {
+                    let data;
+                    if (cell.column.Header === t('name')) {
+                      data = (
+                        <Flex>
+                          <Image
+                            borderRadius="0px"
+                            objectFit="cover"
+                            src={cell.value.image}
+                            w="85px"
+                            h="62px"
+                            mr="3"
+                          />
+                          <Box>
+                            <Text color="text-basic" textStyle="h3">
+                              {cell.value.name}
+                            </Text>
+                            <Text textStyle="h2" color="text-body" mt="1">
+                              {cell.value.code}
+                            </Text>
+                          </Box>
+                        </Flex>
+                      );
+                    } else if (cell.column.Header === t('industry')) {
+                      data = (
+                        <Text color="text-basic" textStyle="h3">
+                          {cell.value}
+                        </Text>
+                      );
+                    } else if (cell.column.Header === t('unit')) {
+                      data = (
+                        <Text color="text-basic" textStyle="h3">
+                          {cell.value}
+                        </Text>
+                      );
+                    } else if (cell.column.Header === t('price')) {
+                      data = (
+                        <Text color="text-basic" textStyle="h3">
+                          {cell.value}đ
+                        </Text>
+                      );
+                    } else if (cell.column.Header === t('origin')) {
+                      data = (
+                        <Text color="text-basic" textStyle="h3">
+                          {cell.value}
+                        </Text>
+                      );
+                    } else if (cell.column.Header === t('brand')) {
+                      data = (
+                        <Text color="text-basic" textStyle="h3">
+                          {cell.value.name}
+                        </Text>
+                      );
+                    }
+                    return (
+                      <Td
+                        {...cell.getCellProps()}
+                        key={index}
+                        fontSize={{ sm: '14px' }}
+                        minW={{ sm: '150px', md: '200px', lg: 'auto' }}
+                        borderColor="transparent">
+                        {data}
+                      </Td>
                     );
-                  } else if (cell.column.Header === t('industry')) {
-                    data = (
-                      <Text color="text-basic" textStyle="h3">
-                        {cell.value}
-                      </Text>
-                    );
-                  } else if (cell.column.Header === t('unit')) {
-                    data = (
-                      <Text color="text-basic" textStyle="h3">
-                        {cell.value}
-                      </Text>
-                    );
-                  } else if (cell.column.Header === t('price')) {
-                    data = (
-                      <Text color="text-basic" textStyle="h3">
-                        {cell.value}đ
-                      </Text>
-                    );
-                  } else if (cell.column.Header === t('origin')) {
-                    data = (
-                      <Text color="text-basic" textStyle="h3">
-                        {cell.value}
-                      </Text>
-                    );
-                  } else if (cell.column.Header === t('brand')) {
-                    data = (
-                      <Text color="text-basic" textStyle="h3">
-                        {cell.value.name}
-                      </Text>
-                    );
-                  }
-                  return (
-                    <Td
-                      {...cell.getCellProps()}
-                      key={index}
-                      fontSize={{ sm: '14px' }}
-                      minW={{ sm: '150px', md: '200px', lg: 'auto' }}
-                      borderColor="transparent">
-                      {data}
-                    </Td>
-                  );
-                })}
-              </Tr>
-            );
-          })}
+                  })}
+                </Tr>
+              );
+            })}
         </Tbody>
       </Table>
     </Box>

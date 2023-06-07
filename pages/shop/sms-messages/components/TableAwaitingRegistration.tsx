8f3f2af602c5f5198be5771e4a0c5929c386f4f5
Drakulus <khuatdangquang11a9@gmail.com>
2023-06-07 10:41:47 +0700

    fix jenkin

--- pages/shop/sms-messages/components/TableAwaitingRegistration.tsx
+++ pages/shop/sms-messages/components/TableAwaitingRegistration.tsx
@@ -25,7 +25,40 @@ import Images from 'assets';
 import dayjs from 'dayjs';
 import SmsMessageItem from './SmsMessageItem';
 import { PaginationPanel, RangeDatePickerItem } from 'components';
-import { data_awaiting_registration } from '../data';
+import { ESMSStatus } from 'constants/types';
+
+const data_awaiting_registration = [
+  {
+    image:
+      'https://user-images.githubusercontent.com/42206067/243565384-c6f7df0a-8d76-43c9-82d0-0c41e72ffd23.png',
+    code: 'SMS01',
+    quantity: 30,
+    startDate: dayjs().unix(),
+    endDate: dayjs().add(2, 'months').unix(),
+    price: 50000,
+    status: ESMSStatus.AWAITING_REGISTRATION,
+  },
+  {
+    image:
+      'https://user-images.githubusercontent.com/42206067/243565384-c6f7df0a-8d76-43c9-82d0-0c41e72ffd23.png',
+    code: 'SMS01',
+    quantity: 30,
+    startDate: dayjs().unix(),
+    endDate: dayjs().add(2, 'months').unix(),
+    price: 50000,
+    status: ESMSStatus.AWAITING_REGISTRATION,
+  },
+  {
+    image:
+      'https://user-images.githubusercontent.com/42206067/243565384-c6f7df0a-8d76-43c9-82d0-0c41e72ffd23.png',
+    code: 'SMS01',
+    quantity: 30,
+    startDate: dayjs().unix(),
+    endDate: dayjs().add(2, 'months').unix(),
+    price: 50000,
+    status: ESMSStatus.AWAITING_REGISTRATION,
+  },
+];
 
 const TableAwaitingRegistration = () => {
   const router = useRouter();
